935e78341d89c77c184e31266135c5ca0500665e
Ryan Levick <ryan.levick@gmail.com>
2019-04-07 16:43:44 +0200

    js First version

---
+++ crates/rustdoku-js/javascript-version.js
@@ -0,0 +1,236 @@
+function generateJs(n) {
+  const b = full()
+  remove(b, n)
+  return b
+}
+function full() {
+  const board = Array(81).fill(undefined)
+  solve(board)
+  return board
+}
+
+function remove(board, n) {
+  for (let i = 0; i < n; i++) {
+    let tries = 0
+    while (true) {
+      const index = Math.floor(Math.random() * 81)
+      const original = board[index]
+      if (original !== undefined) {
+        const clone1 = board.slice(0)
+        const clone2 = board.slice(0)
+        clone1[index] = undefined
+        clone2[index] = undefined
+        const condition = [index, original]
+        const withCondition = solve(clone1, condition)
+        const withoutCondition = solve(clone2)
+        if (!withCondition && withoutCondition) {
+          board[index] = undefined
+          break
+        } else {
+          tries++
+          if (tries >= 1000) {
+            throw toString(clone1)
+          }
+        }
+      }
+    }
+
+  }
+
+}
+function solve(board, condition) {
+  return check(board, 0, condition)
+}
+
+function check(board, index, condition) {
+  if (index > 80) { return true }
+
+  const original = board[index]
+  const possibleValues = getPossibleValues(board, index)
+
+  while (true) {
+    if (possibleValues.every(x => x === false)) { break }
+    const testIndex = Math.floor(Math.random(1) * 9)
+    const isPossible = possibleValues[testIndex]
+
+    if (!isPossible) {
+      continue
+    }
+
+    const value = testIndex + 1
+
+    const is_forbidden = condition && condition[0] === index && condition[1] === value
+
+    if (!is_forbidden) {
+      board[index] = value;
+      if (check(board, index + 1, condition)) {
+        return true
+      }
+    }
+
+    possibleValues[testIndex] = false
+  }
+
+  board[index] = original
+  return false
+}
+
+function getPossibleValues(board, index) {
+  const value = board[index]
+  if (value) {
+    const possibilities = Array(9).fill(false)
+    possibilities[value - 1] = true
+    return possibilities
+  }
+
+  const rowIndex = Math.floor(index / 9)
+  const columnIndex = index % 9
+  const squareIndex = (Math.floor(rowIndex / 3) * 3) + Math.floor(columnIndex / 3)
+
+  const row = getRow(board, rowIndex)
+  const column = getColumn(board, columnIndex)
+  const square = getSquare(board, squareIndex)
+  const possibilities = Array(9).fill(true)
+
+  for (rowValue of row) {
+    if (rowValue !== undefined) {
+      possibilities[rowValue - 1] = false
+    }
+  }
+
+  for (columnValue of column) {
+    if (columnValue !== undefined) {
+      possibilities[columnValue - 1] = false
+    }
+  }
+
+  for (squareValue of square) {
+    if (squareValue !== undefined) {
+      possibilities[squareValue - 1] = false
+    }
+  }
+
+  return possibilities
+}
+
+function getRow(board, rowIndex) {
+  let start = rowIndex * 9
+  let end = start + 9
+
+  return board.slice(start, end)
+}
+
+function getColumn(board, columnIndex) {
+  const values = Array(9)
+
+  for (let i = 0; i < 9; i++) {
+    const j = (i * 9) + columnIndex
+    values[i] = board[j]
+  }
+
+  return values
+}
+
+function getSquare(board, squareIndex) {
+  const verticalOffset = Math.floor(squareIndex / 3)
+  const horizontalOffset = Math.floor(squareIndex % 3)
+
+  const offset = (verticalOffset * 27) + (horizontalOffset * 3)
+
+  const values = Array(9).fill(undefined)
+
+  for (let level = 0; level < 3; level++) {
+    const from = offset + (level * 9)
+    const to = from + 3
+    let i = 0
+    for (val of board.slice(from, to)) {
+      values[i + (level * 3)] = val
+      i++
+    }
+  }
+
+  return values
+}
+
+function toString(board) {
+  let output = ""
+  let i = 0;
+  for (value of board) {
+    if (value) {
+      output += value
+    } else {
+
+      output += "*"
+    }
+
+    if (!((i + 1) % 9 == 0)) {
+      output += " "
+    } else {
+      output += "\n"
+    }
+    i++
+  }
+
+  return output
+
+}
+
+
+function newBoard() {
+  const board = Array(81).fill(undefined)
+
+  // Hard
+  board[1] = 2
+  board[2] = 5
+  board[7] = 6
+  board[9] = 1
+  board[15] = 7
+  board[16] = 9
+  board[18] = 4
+  board[23] = 1
+  board[29] = 9
+  board[33] = 6
+  board[34] = 8
+  board[41] = 9
+  board[43] = 5
+  board[46] = 1
+  board[48] = 7
+  board[50] = 3
+  board[54] = 8
+  board[59] = 5
+  board[66] = 8
+  board[67] = 7
+  board[69] = 5
+  board[71] = 2
+  board[74] = 7
+  board[75] = 2
+
+  //Easy
+  // board[2] = 2
+  // board[11] = 5
+  // board[12] = 8
+  // board[14] = 2
+  // board[15] = 9
+  // board[19] = 8
+  // board[21] = 4
+  // board[22] = 6
+  // board[25] = 3
+  // board[27] = 9
+  // board[31] = 7
+  // board[33] = 2
+  // board[35] = 6
+  // board[39] = 9
+  // board[41] = 1
+  // board[55] = 6
+  // board[57] = 1
+  // board[63] = 1
+  // board[64] = 7
+  // board[67] = 5
+  // board[70] = 4
+  // board[71] = 8
+  // board[72] = 2
+  // board[75] = 3
+  // board[76] = 4
+  // board[77] = 6
+  return board
+}
---
+++ crates/rustdoku-js/src/lib.rs
@@ -0,0 +1,112 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub struct Game {
+    inner: sudoku::Game,
+}
+
+#[wasm_bindgen]
+impl Game {
+    #[wasm_bindgen(constructor)]
+    pub fn new(count_missing: u8) -> Game {
+        let mut game = Game::new_full();
+        game.empty(count_missing);
+        game
+    }
+
+    #[wasm_bindgen(js_name = newFull)]
+    pub fn new_full() -> Game {
+        let mut game = sudoku::Game::new([None; 81]);
+        assert!(game.solve());
+        Game { inner: game }
+    }
+
+    #[wasm_bindgen(js_name = newEasy)]
+    pub fn new_easy() -> Game {
+        let mut numbers = [None; 81];
+        numbers[2] = Some(2);
+        numbers[11] = Some(5);
+        numbers[12] = Some(8);
+        numbers[14] = Some(2);
+        numbers[15] = Some(9);
+        numbers[19] = Some(8);
+        numbers[21] = Some(4);
+        numbers[22] = Some(6);
+        numbers[25] = Some(3);
+        numbers[27] = Some(9);
+        numbers[31] = Some(7);
+        numbers[33] = Some(2);
+        numbers[35] = Some(6);
+        numbers[39] = Some(9);
+        numbers[41] = Some(1);
+        numbers[55] = Some(6);
+        numbers[57] = Some(1);
+        numbers[63] = Some(1);
+        numbers[64] = Some(7);
+        numbers[67] = Some(5);
+        numbers[70] = Some(4);
+        numbers[71] = Some(8);
+        numbers[72] = Some(2);
+        numbers[75] = Some(3);
+        numbers[76] = Some(4);
+        numbers[77] = Some(6);
+        let inner = sudoku::Game::new(numbers);
+        Game { inner }
+    }
+
+    #[wasm_bindgen(js_name = newHard)]
+    pub fn new_hard() -> Game {
+        let mut numbers = [None; 81];
+        numbers[1] = Some(2);
+        numbers[2] = Some(5);
+        numbers[7] = Some(6);
+        numbers[9] = Some(1);
+        numbers[15] = Some(7);
+        numbers[16] = Some(9);
+        numbers[18] = Some(4);
+        numbers[23] = Some(1);
+        numbers[29] = Some(9);
+        numbers[33] = Some(6);
+        numbers[34] = Some(8);
+        numbers[41] = Some(9);
+        numbers[43] = Some(5);
+        numbers[46] = Some(1);
+        numbers[48] = Some(7);
+        numbers[50] = Some(3);
+        numbers[54] = Some(8);
+        numbers[59] = Some(5);
+        numbers[66] = Some(8);
+        numbers[67] = Some(7);
+        numbers[69] = Some(5);
+        numbers[71] = Some(2);
+        numbers[74] = Some(7);
+        numbers[75] = Some(2);
+
+        let inner = sudoku::Game::new(numbers);
+        Game { inner }
+    }
+
+    pub fn empty(&mut self, count: u8) {
+        self.inner.empty(count)
+    }
+
+    pub fn solve(&mut self) -> f64 {
+        let t1 = web_sys::window().unwrap().performance().unwrap().now();
+        self.inner.solve();
+        let t2 = web_sys::window().unwrap().performance().unwrap().now();
+
+        t2 - t1
+    }
+
+    #[wasm_bindgen(js_name = toJs)]
+    pub fn to_js(&self) -> Vec<wasm_bindgen::JsValue> {
+        self.inner
+            .numbers
+            .iter()
+            .map(|n| match n {
+                None => wasm_bindgen::JsValue::UNDEFINED,
+                Some(n) => wasm_bindgen::JsValue::from_f64(*n as f64),
+            })
+            .collect()
+    }
+}
